0e9689c039fd48b2eecb18d16d1bd4dd4d6d6625
shrikant nimhan <shni9045@colorado.edu>
2021-09-13 04:45:27 +0530

    Final Assignment 3 Submission

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -120,7 +120,7 @@ sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-cd $FINDER_APP_DIR
+cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
