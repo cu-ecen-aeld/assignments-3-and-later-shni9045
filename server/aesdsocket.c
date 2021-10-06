56ea95cd0495275d2ed68c98fb155e14b094bac6
shrikant nimhan <shni9045@colorado.edu>
2021-10-06 17:29:39 -0600

    Initial Commit Assignment 6 Part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -21,39 +21,226 @@
 #include <syslog.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "queue.h"
 
 // macro definition for  buffer size
 #define BUFSIZE 500
 // macro definiton for port#
 #define PORT "9000"
 // macro definition for pending connections
-#define BACKLOG 2
+#define BACKLOG 1000
 
-// structure to store file metadata
-struct stat  stat_data;
-int status;
+typedef struct{
 
-// Buffer declarations
-char *read_buf,*write_buf,*temp_buf,*out_buf;
+    pthread_t threaddec;
+    int threadIdx;
+    int sock;
+    char* read_buf;
+    char* write_buf;
+    bool completion_status;
 
+}threadParams_t;
+
+
+
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s{
+
+    threadParams_t threadParams;
+    SLIST_ENTRY(slist_data_s) entries;
+};
+
+
+slist_data_t *datap = NULL;
+SLIST_HEAD(slisthead,slist_data_s) head;
+
+
+
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 pid_t check;
 int sock_t;
+
 int newsock_t;
-
-int fd;
-
-int buff_pos=0;
-int currbuf_size=BUFSIZE;
-
-int num_bytes;
-
-ssize_t read_bytes;
 
 struct addrinfo host;
 struct addrinfo *servinfo;
 
 struct sockaddr_in con_addr;
+
+
+void close_graceful(){
+
+      // close listening server socket fd
+    close(sock_t);
+
+    // close file descriptor
+    //close(fd);
+
+    // Delete file
+    if(remove("/var/tmp/aesdsocketdata") != 0){
+        syslog(LOG_ERR,"\nERROR in deleting file");
+    }
+    
+
+    
+    // Cancel threads &free pointers
+    SLIST_FOREACH(datap,&head,entries){
+
+        if (datap->threadParams.completion_status != true){
+
+            pthread_cancel(datap->threadParams.threaddec);
+            free(datap->threadParams.read_buf);
+            free(datap->threadParams.write_buf);
+            
+        }
+
+
+    }
+
+     // close log
+    closelog();
+    
+
+
+}
+
+
+
+void Send_Receive(void *threadp){
+
+    int fd;
+
+    char *ch;
+
+    threadParams_t *threadsock = (threadParams_t*)threadp;
+
+    // structure to store file metadata
+    struct stat  stat_data;
+    int status;
+
+
+    int num_bytes,wbytes;
+    int buff_pos=0;
+
+
+    ssize_t read_bytes;
+
+    int currbuf_size=BUFSIZE;
+
+    // Buffer declarations
+    char *temp_buf,*out_buf;
+
+    // allocate read write buffers
+    threadsock->read_buf = (char*)malloc(sizeof(char)*BUFSIZE);
+    threadsock->write_buf = (char*)malloc(sizeof(char)*BUFSIZE);
+
+
+    // Read till packet is complete
+    while((num_bytes = recv(newsock_t,threadsock->read_buf+buff_pos, BUFSIZE, 0))>0){
+
+    if(num_bytes == -1){
+        perror("ERROR recv():");
+        close_graceful();
+        exit(-1);    
+
+    }
+
+    buff_pos += num_bytes;
+    
+    // dynamicall increase buffer size for incoming packets
+    //if (buff_pos >= currbuf_size){
+
+    currbuf_size+=BUFSIZE;
+
+    temp_buf = realloc(threadsock->read_buf,sizeof(char)*currbuf_size);
+
+    if(temp_buf == NULL){
+        syslog(LOG_ERR,"\nErrror in realloc");
+        close_graceful();
+        exit(-1);
+    }
+
+    else threadsock->read_buf = temp_buf;
+    
+    //}
+    
+    // Search for NULL character
+    ch = strchr(threadsock->read_buf,'\n');
+
+    if(ch != NULL) break;
+    
+    
+    }
+
+    /*
+    // Block signals to avoid partial write
+    if (sigprocmask(SIG_BLOCK,&set,NULL) == -1){
+        perror("\nERROR sigprocmask():");
+        close_graceful();
+        exit(-1);
+    }*/
+
+    // mutex lock
+    pthread_mutex_lock(&file_mutex);
+
+    // Open file for writing
+    fd = open("/var/tmp/aesdsocketdata",O_RDWR|O_CREAT|O_APPEND,S_IRWXU);
+    if(fd<0){
+        perror("\nERROR open():");
+        close_graceful();
+        exit(-1);
+    }
+
+    // Write to file
+    wbytes = write(fd,threadsock->read_buf,buff_pos);
+    if (wbytes == -1){
+        perror("\nERROR write():");
+        close_graceful();
+        exit(-1);
+    }
+    
+    // Extract file size
+    status = stat("/var/tmp/aesdsocketdata",&stat_data);
+    int si;
+    if (status == 0){
+    si=stat_data.st_size;
+
+    }
+
+    out_buf=realloc(threadsock->write_buf,sizeof(char)*si);
+    threadsock->write_buf=out_buf;
+
+
+    lseek(fd,0,SEEK_SET);
+
+    read_bytes = read(fd,threadsock->write_buf,si);
+
+    pthread_mutex_unlock(&file_mutex);
+    
+    // Send packets
+    if (send(newsock_t,threadsock->write_buf,read_bytes, 0) == -1){
+    perror("send");
+    close_graceful();
+    }
+
+    /*// Unblock signals to avoid partial write
+    if (sigprocmask(SIG_UNBLOCK,&set,NULL) == -1){
+        perror("\nERROR sigprocmask():");
+        close_graceful();
+        exit(-1);
+    }*/
+
+    close(threadsock->sock);
+
+    //status true
+    threadsock->completion_status = true;
+
+    free(threadsock->read_buf);
+    free(threadsock->write_buf);
+}
 
 static void sig_handler(int signo){
 
@@ -61,56 +248,41 @@ static void sig_handler(int signo){
     // close listening server socket fd
     close(sock_t);
 
-    close(newsock_t);
-
     // close file descriptor
-    close(fd);
+    //close(fd);
 
     // Delete file
     if(remove("/var/tmp/aesdsocketdata") != 0){
         syslog(LOG_ERR,"\nERROR in deleting file");
     }
     
-    // close log
-    closelog();
     
-    // free pointers
-    free(read_buf);
-    free(write_buf);
+    // Cancel threads &free pointers
+    SLIST_FOREACH(datap,&head,entries){
+
+        if (datap->threadParams.completion_status != true){
+
+            pthread_cancel(datap->threadParams.threaddec);
+            free(datap->threadParams.read_buf);
+            free(datap->threadParams.write_buf);
+            
+        }
+
+
+    }
+
+     // close log
+    closelog();
     
     exit(0);
 
 
 }
 
-void close_graceful(){
-
-      // close listening server socket fd
-    close(sock_t);
-
-    close(newsock_t);
-
-    // close file descriptor
-    close(fd);
-
-    // Delete file
-    if(remove("/var/tmp/aesdsocketdata") != 0){
-        syslog(LOG_ERR,"\nERROR in deleting file");
-    }
-    
-    // close log
-    closelog();
-    
-    // free pointers
-    free(read_buf);
-    free(write_buf);
-    
-
-
-}
-
 int main(int argc, char* argv[])
 {
+
+    SLIST_INIT(&head);
 
 
     socklen_t len;
@@ -207,10 +379,6 @@ int main(int argc, char* argv[])
         exit(-1);
 
     }
-
-    // allocate memory for buffers
-    read_buf = (char*)malloc(sizeof(char)*BUFSIZE);
-    write_buf = (char*)malloc(sizeof(char)*BUFSIZE);
     
     // if correct parameter is passed to code start daemon
     if (argc == 2){
@@ -240,8 +408,8 @@ int main(int argc, char* argv[])
     }
 
     }
-        
 
+    //int num_threads = 0;
 
     while(1) {
 
@@ -254,110 +422,34 @@ int main(int argc, char* argv[])
     if (newsock_t == -1 ){
         perror("\nERROR accept():");
         //continue;
-        }
+    }
 
 
     char *ip_string = inet_ntoa(con_addr.sin_addr);
     //printf("\nAccepted connection from %s",ip_string);
     syslog(LOG_DEBUG,"\nAccepted connection from %s",ip_string);
-    
-
-    char *ch;
-    ssize_t wbytes;
-
-    buff_pos=0;
-
-    // Read till packet is complete
-    while((num_bytes = recv(newsock_t,read_buf+buff_pos, BUFSIZE, 0))>0){
-
-    if(num_bytes == -1){
-        perror("ERROR recv():");
-        close_graceful();
-        exit(-1);    
-
-    }
-
-    buff_pos += num_bytes;
-    
-    // dynamicall increase buffer size for incoming packets
-    //if (buff_pos >= currbuf_size){
-
-    currbuf_size+=BUFSIZE;
-
-    temp_buf = realloc(read_buf,sizeof(char)*currbuf_size);
-
-    if(temp_buf == NULL){
-        syslog(LOG_ERR,"\nErrror in realloc");
-        close_graceful();
-        exit(-1);
-    }
-
-    else read_buf = temp_buf;
-    
-    //}
-    
-    // Search for NULL character
-    ch = strchr(read_buf,'\n');
-
-    if(ch != NULL) break;
-    
-    
-    }
 
 
-    // Block signals to avoid partial write
-    if (sigprocmask(SIG_BLOCK,&set,NULL) == -1){
-        perror("\nERROR sigprocmask():");
-        close_graceful();
-        exit(-1);
-    }
+    datap = malloc(sizeof(slist_data_t));
+    SLIST_INSERT_HEAD(&head,datap,entries);
+    datap->threadParams.sock = newsock_t;
+    pthread_create(&(datap->threadParams.threaddec),(void*)0,(void*)&Send_Receive,(void*)&(datap->threadParams));
 
-    // Open file for writing
-    fd = open("/var/tmp/aesdsocketdata",O_RDWR|O_CREAT|O_APPEND,S_IRWXU);
-    if(fd<0){
-        perror("\nERROR open():");
-        close_graceful();
-        exit(-1);
-    }
+    SLIST_FOREACH(datap,&head,entries){
 
-    // Write to file
-    wbytes = write(fd,read_buf,buff_pos);
-    if (wbytes == -1){
-        perror("\nERROR write():");
-        close_graceful();
-        exit(-1);
-    }
-    
-    // Extract file size
-    status = stat("/var/tmp/aesdsocketdata",&stat_data);
-    int si;
-    if (status == 0){
-    si=stat_data.st_size;
+        if(datap->threadParams.completion_status == false){
+
+            continue;
+
+        }
+
+        else if (datap->threadParams.completion_status == true){
+
+            pthread_join(datap->threadParams.threaddec,NULL);
+
+        }
 
     }
-
-    out_buf=realloc(write_buf,sizeof(char)*si);
-    write_buf=out_buf;
-
-
-    lseek(fd,0,SEEK_SET);
-
-    read_bytes = read(fd,write_buf,si);
-    
-    // Send packets
-    if (send(newsock_t,write_buf,read_bytes, 0) == -1){
-    perror("send");
-    close_graceful();
-    }
-
-    // Unblock signals to avoid partial write
-    if (sigprocmask(SIG_UNBLOCK,&set,NULL) == -1){
-        perror("\nERROR sigprocmask():");
-        close_graceful();
-        exit(-1);
-    }
-
-    close(newsock_t);
 
 }
 
